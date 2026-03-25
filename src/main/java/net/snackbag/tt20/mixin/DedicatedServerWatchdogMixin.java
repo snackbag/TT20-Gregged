a524275eaee261e0a1383736ec7ab69c835612e7
JXSnack <jx.snack@gmail.com>
2026-03-25 11:57:09 +0100

    port watchdog timeout stopper

--- src/main/java/net/snackbag/tt20/mixin/DedicatedServerWatchdogMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/DedicatedServerWatchdogMixin.java
@@ -1,20 +1,28 @@
 package net.snackbag.tt20.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import net.minecraft.server.dedicated.ServerWatchdog;
+import net.minecraft.server.dedicated.ServerHangWatchdog;
 import net.snackbag.tt20.TT20;
 import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(ServerWatchdog.class)
+@Mixin(ServerHangWatchdog.class)
 public abstract class DedicatedServerWatchdogMixin {
-    //? if >=1.20.6 {
-    /*@ModifyExpressionValue(method = "run", at = @At(value = "FIELD", target = "Lnet/minecraft/server/dedicated/ServerWatchdog;maxTickTimeNanos:J", opcode = Opcodes.GETFIELD))
-    *///?} else {
-    @ModifyExpressionValue(method = "run", at = @At(value = "FIELD", target = "Lnet/minecraft/server/dedicated/ServerWatchdog;maxTickTime:J", opcode = Opcodes.GETFIELD))
-    //?}
-    private long cancelServerWatchdog(long original) {
-        return TT20.config.serverWatchdog() ? original : Long.MAX_VALUE; // using Long.MAX_VALUE, because we can't cancel the if statement
+    @Final
+    @Shadow
+    private long maxTickTime; // because it's private
+
+    @Redirect(
+            method = "run",
+            at = @At(
+                    value = "FIELD",
+                    target = "Lnet/minecraft/server/dedicated/ServerHangWatchdog;maxTickTime:J",
+                    opcode = Opcodes.GETFIELD)
+    )
+    private long modifyMaxTickTime(ServerHangWatchdog instance) {
+        return TT20.config.serverWatchdog() ? this.maxTickTime : Long.MAX_VALUE;
     }
 }
