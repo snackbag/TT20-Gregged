497232d4adf6203798132eeab4a2be1ff1ae6629
JXSnack <jx.snack@gmail.com>
2026-03-25 12:31:22 +0100

    mark as unfinished

--- src/main/java/net/snackbag/tt20/mixin/fluid/WaterFluidMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/fluid/WaterFluidMixin.java
@@ -1,17 +1,19 @@
-package net.snackbag.tt20.mixin.fluid;
+// TODO: make this work with any modded fluid before implementing
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.minecraft.world.level.material.WaterFluid;
-import net.snackbag.tt20.TT20;
-import net.snackbag.tt20.util.TPSUtil;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-
-@Mixin(WaterFluid.class)
-public abstract class WaterFluidMixin {
-    @ModifyReturnValue(method = "getTickDelay", at = @At("RETURN"))
-    private int tickRateTT20(int original) {
-        if (!TT20.config.enabled() || !TT20.config.fluidAcceleration()) return original;
-        return TPSUtil.tt20(original, true);
-    }
-}
+//package net.snackbag.tt20.mixin.fluid;
+//
+//import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+//import net.minecraft.world.level.material.WaterFluid;
+//import net.snackbag.tt20.TT20;
+//import net.snackbag.tt20.util.TPSUtil;
+//import org.spongepowered.asm.mixin.Mixin;
+//import org.spongepowered.asm.mixin.injection.At;
+//
+//@Mixin(WaterFluid.class)
+//public abstract class WaterFluidMixin {
+//    @ModifyReturnValue(method = "getTickDelay", at = @At("RETURN"))
+//    private int tickRateTT20(int original) {
+//        if (!TT20.config.enabled() || !TT20.config.fluidAcceleration()) return original;
+//        return TPSUtil.tt20(original, true);
+//    }
+//}
