dc499181e623df8d69b49e13e0955d8d95a67658
JXSnack <jx.snack@gmail.com>
2026-03-25 11:29:33 +0100

    port eating acceleration

--- src/main/java/net/snackbag/tt20/mixin/item/ItemMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/item/ItemMixin.java
@@ -1,17 +1,21 @@
 package net.snackbag.tt20.mixin.item;
 
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
-import net.minecraft.world.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
 import net.snackbag.tt20.TT20;
 import net.snackbag.tt20.util.TPSUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(Item.class)
+@Mixin(ItemFood.class)
 public abstract class ItemMixin {
-    @ModifyReturnValue(method = "getUseDuration", at = @At("RETURN"))
-    private int onGetMaxUseTime(int original) {
-        if (!TT20.config.enabled() || !TT20.config.eatingAcceleration() || original == 0) return original;
-        return TPSUtil.tt20(original, true);
+    @Inject(method = "getMaxItemUseDuration", at = @At("RETURN"), cancellable = true)
+    private void onGetMaxUseTime(ItemStack stack, CallbackInfoReturnable<Integer> cir) {
+        int original = cir.getReturnValue();
+
+        if (!TT20.config.enabled() || !TT20.config.eatingAcceleration() || original == 0) return;
+        cir.setReturnValue(TPSUtil.tt20(original, true));
     }
 }
