6937be26b50eb76807ae36a35d6b3071d4cef37f
JXSnack <jx.snack@gmail.com>
2026-03-25 12:25:34 +0100

    port warning message

--- src/main/java/net/snackbag/tt20/mixin/client/IntegratedServerMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/client/IntegratedServerMixin.java
@@ -1,15 +1,15 @@
 package net.snackbag.tt20.mixin.client;
 
-import net.minecraft.client.server.IntegratedServer;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraft.server.integrated.IntegratedServer;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.snackbag.tt20.TT20;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@OnlyIn(Dist.CLIENT)
+@SideOnly(Side.CLIENT)
 @Mixin(IntegratedServer.class)
 public abstract class IntegratedServerMixin {
     @Inject(method = "stopServer", at = @At("HEAD"))
