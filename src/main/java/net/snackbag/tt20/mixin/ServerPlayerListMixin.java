02f3ce9c3d951a30bf2e9a782a4cade135cfd068
JXSnack <jx.snack@gmail.com>
2026-03-25 12:29:24 +0100

    automatic updater

--- src/main/java/net/snackbag/tt20/mixin/ServerPlayerListMixin.java
+++ src/main/java/net/snackbag/tt20/mixin/ServerPlayerListMixin.java
@@ -1,35 +1,31 @@
 package net.snackbag.tt20.mixin;
 
-import net.minecraft.network.Connection;
-import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-//? if >=1.20.6 {
-/*import net.minecraft.server.network.CommonListenerCookie;
-*///?}
-import net.minecraft.server.players.PlayerList;
+import net.minecraft.network.NetHandlerPlayServer;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.server.management.PlayerList;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.util.text.TextComponentString;
+
 import net.snackbag.tt20.ModUpdater;
 import net.snackbag.tt20.TT20;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
-
 @Mixin(PlayerList.class)
 public abstract class ServerPlayerListMixin {
-    @Inject(method = "placeNewPlayer", at = @At("TAIL"))
 
-    //? if >=1.20.6 {
-    /*private void sendPlayerUpdateMessageIfCorrectPermissions(Connection connection, ServerPlayer player, CommonListenerCookie listener, CallbackInfo ci) {
-    *///?} else {
-    private void sendPlayerUpdateMessageIfCorrectPermissions(Connection connection, ServerPlayer player, CallbackInfo ci) {
-        //?}
+    @Inject(method = "initializeConnectionToPlayer", at = @At("TAIL"))
+    private void sendPlayerUpdateMessageIfCorrectPermissions(NetworkManager netManager, EntityPlayerMP playerIn, NetHandlerPlayServer nethandlerplayserver, CallbackInfo ci) {
         if (!TT20.config.automaticUpdater() || !ModUpdater.hasUpdate) return;
 
-        if (Objects.requireNonNull(player.getServer()).getPlayerList().isOp(player.getGameProfile())) {
-            player.sendSystemMessage(Component.literal(ModUpdater.updateMessage));
-            player.sendSystemMessage(Component.literal("§oOnly operators can see this message"));
+        PlayerList list = playerIn.mcServer.getPlayerList();
+
+        if (list.canSendCommands(playerIn.getGameProfile())) {
+            playerIn.sendMessage(new TextComponentString(ModUpdater.updateMessage));
+            playerIn.sendMessage(new TextComponentString("§oOnly operators can see this message"));
         }
     }
 }
